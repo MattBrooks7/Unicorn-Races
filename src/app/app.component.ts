f5114b0bc986ca4ad821bf02d30d6baa0f411bb2
TotallyMoxie <134233554+TotallyMoxie@users.noreply.github.com>
2023-11-30 20:05:25 -0600

    Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RacesService } from './races.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Unicorns';
+
+  constructor(private racesService: RacesService) { }
+
+  makerace(){
+    this.racesService.createRace();
+    return this.racesService.UnicornList;
+  }
 }
--- src/app/races.service.ts
+++ src/app/races.service.ts
@@ -4,28 +4,34 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 
+
 export class RacesService {
 
-  Unicorn = new Unicorn();
-  race = new Races();
+    Unicorn = {id: 0, name: 'here is a unicorn'};
+    race = new Races();
+    UnicornList: Unicorns[] = [];
 
-  constructor() {
+    constructor() {
 
+    }
+
+    createRace(){
+      let UnicornList = new Unicorns();
+      this.UnicornList.push(this.race);
+      console.log(this.race);
+    }
+
+}
+
+class Unicorns {
+    id: number = {} as number;
+    name: string = '';
   }
-}
+  class Races {
+    id: number = {} as number;
+    name: string = '';
+    unicorns: Unicorns[]= [];
+  }
 
 
-class Unicorn {
-  id: number = {} as number;
-  name: string = '';
-}
 
-class Races {
-  id: number = {} as number;
-  name: string = '';
-  unicorns: Unicorn[]= [];
-}
-
-createRace() {
-
-}
